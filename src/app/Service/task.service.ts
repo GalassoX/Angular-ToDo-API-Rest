669ea49c39ea1192b41eda619df1465f4a23af0c
Rafael <68511324+GalassoX@users.noreply.github.com>
2023-02-03 13:19:40 -0500

    Add HttpResponse to best error handling

--- src/app/Service/task.service.ts
+++ src/app/Service/task.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ITask } from 'app/models/Task';
 
@@ -22,25 +22,25 @@ export class TaskService {
   url = 'http://localhost:8080/tasks';
 
   getTasks() {
-    return this.http.get<ITask[]>(this.url);
+    return this.http.get<HttpResponse<ITask[]>>(this.url);
   }
 
   getTaskById(id: string) {
-    return this.http.get<ITask>(`${this.url}/${id}`);
+    return this.http.get<HttpResponse<ITask>>(`${this.url}/${id}`);
   }
 
   createTask(body: IBodyCreateTask) {
     if (!body || !body.title || !body.description) {
       return null;
     }
-    return this.http.post<ICreatedTask>(this.url, body);
+    return this.http.post<HttpResponse<ICreatedTask>>(this.url, body);
   }
 
   editTask(id: string, body: IBodyCreateTask) {
-    return this.http.put<ICreatedTask>(`${this.url}/${id}`, body);
+    return this.http.put<HttpResponse<ICreatedTask>>(`${this.url}/${id}`, body);
   }
 
   deleteTask(id: string) {
-    return this.http.delete<{ message: string }>(`${this.url}/${id}`);
+    return this.http.delete<HttpResponse<{ message: string }>>(`${this.url}/${id}`);
   }
 }
